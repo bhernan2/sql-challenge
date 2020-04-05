8fc42456ebe28bd4c0ae61e8884826388213e7aa
Bianca A. Hernandez <bhernan2@gmail.com>
2020-04-05 11:15:19 -0500

    created department managers table

--- EmployeeSQL/employees.sql
+++ EmployeeSQL/employees.sql
@@ -8,10 +8,29 @@ DROP TABLE IF EXISTS salaries;
 DROP TABLE IF EXISTS titles;
 
 --import CSV files into above tables
+--departments
 CREATE TABLE "departments" (
-    "dept_no" VARCHAR   NOT NULL,
-    "dept_name" VARCHAR   NOT NULL,
+    "dept_no" VARCHAR NOT NULL,
+    "dept_name" VARCHAR NOT NULL,
     CONSTRAINT "pk_departments" PRIMARY KEY (
         "dept_no"
      )
 );
+--department employees
+CREATE TABLE "dept_emp" (
+    "emp_no" INT   NOT NULL,
+    "dept_no" VARCHAR NOT NULL,
+    "from_date" DATE NOT NULL,
+    "to_date" DATE NOT NULL
+);
+--department managers
+CREATE TABLE "dept_managers" (
+    "dept_no" VARCHAR NOT NULL,
+    "emp_no" INT NOT NULL,
+    "from_date" DATE NOT NULL,
+    "to_date" DATE NOT NULL,
+
+);
+
+
+
