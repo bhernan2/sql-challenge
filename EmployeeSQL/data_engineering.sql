4aeb25d79cc221e4a3132132fbf7a35976748b2f
Bianca A. Hernandez <bhernan2@gmail.com>
2020-04-06 16:52:53 -0500

    updated

---
+++ EmployeeSQL/data_engineering.sql
@@ -0,0 +1,72 @@
+--table schemas
+--drop table if it already exists
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS titles;
+
+--import CSV files into above tables
+--departments
+CREATE TABLE "departments" (
+    "dept_no" VARCHAR NOT NULL,
+    "dept_name" VARCHAR NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+--department employees
+CREATE TABLE "dept_emp" (
+    "emp_no" INT   NOT NULL,
+    "dept_no" VARCHAR NOT NULL,
+    "from_date" DATE NOT NULL,
+    "to_date" DATE NOT NULL
+);
+
+--department managers
+CREATE TABLE "dept_manager" (
+	"dept_no" VARCHAR NOT NULL,
+	"emp_no" INT NOT NULL,
+	"from_date" DATE NOT NULL,
+	"to_date" DATE NOT NULL
+);
+
+--employees
+CREATE TABLE "employees" (
+    "emp_no" VARCHAR NOT NULL,
+    "birth_date" DATE NOT NULL,
+    "first_name" VARCHAR NOT NULL,
+    "last_name" VARCHAR NOT NULL,
+    "gender" VARCHAR NOT NULL,
+    "hire_date" DATE NOT NULL
+);
+
+--salaries
+CREATE TABLE "salaries" (
+    "emp_no" INT   NOT NULL,
+    "salary" INT   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL
+);
+
+--titles 
+CREATE TABLE "titles" (
+    "emp_no" INT   NOT NULL,
+    "title" VARCHAR   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL
+);
+
+--import data & check tables 
+SELECT * FROM departments;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM titles;
+
+
+
+
