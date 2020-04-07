9edd2120a27fe417e1d424e82499daa2bd4b14b9
Bianca A. Hernandez <bhernan2@gmail.com>
2020-04-07 08:09:42 -0500

    queries 4-5

--- EmployeeSQL/data_analysis.sql
+++ EmployeeSQL/data_analysis.sql
@@ -21,3 +21,17 @@ JOIN dept_manager
 ON departments.dept_no = dept_manager.dept_no
 JOIN employees
 ON dept_manager.emp_no = employees.emp_no;
+
+--4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+JOIN departments
+ON dept_emp.dept_no = departments.dept_no;
+--5. ist all employees whose first name is "Hercules" and last names begin with "B."
+SELECT first_name, last_name
+FROM employees
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
+--6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
